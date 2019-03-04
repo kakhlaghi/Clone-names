996643e170d592f43b7d08d3869c9b1ca945c821
kakhlaghi <kevin.akhlaghi@gmail.com>
2019-03-04 08:19:05 -0800

    adds nomatch page for incorrect URLs

---
+++ src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NoMatch = () => {
+    return(
+        <div className='App'>
+            <h1> WHOA THERE BUDDY </h1>
+            <img src='https://giphy.com/gifs/Y69ObI4RR1JHW/html5' alt="warf!" />
+            <p> Looks like you found an area that hasn't been developed yet! Let me know and I'll see what I can do! </p>
+            <p> In the mean time, feel free to head home. </p>
+        </div>    
+    )
+}
